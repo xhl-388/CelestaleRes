edefd813cb302a7ea9b0a5fb807f5870628fc658
mhggpo <mhggpo@hotmail.com>
2022-04-14 22:08:20 +0800

    add
    Add HPBarUI

--- Celestale/Assets/Scripts/UI/HPBarUI.cs
+++ Celestale/Assets/Scripts/UI/HPBarUI.cs
@@ -3,35 +3,42 @@ using UnityEngine.UI;
 public class HPBarUI : MonoBehaviour
 {
     public float HP = 1.0f;
+    public float MaxHP = 1.0f;
+    public float x=50.0f;
+    public float y=50.0f;
     private float tmpHP;
 
     private Rect HealthBar;
 
     void Start()
     {
-        HealthBar = new Rect(50, 50, 200, 20);
+        HealthBar = new Rect(x, y, 150, 10);
+        HP = MaxHP;
         tmpHP = HP;
+        Debug.Log(tmpHP+" "+HP+" "+MaxHP);
     }
 
     void OnGUI()
     {
         HP = Mathf.Lerp(HP, tmpHP, 0.05f);
-        GUI.HorizontalScrollbar(HealthBar, 0.0f, HP, 0.0f, 1.0f);
+        GUI.HorizontalScrollbar(HealthBar, 0.0f, HP, 0.0f, MaxHP, GUI.skin.GetStyle("HorizontalScrollbar"));
     }
     public void SetHP(int flag, float damage)
-    {
-        Debug.Log(tmpHP);
+    {        
         if (flag == 1)
         {
             tmpHP += damage;
+            Debug.Log(tmpHP);
         }
         else if (flag == 0)
         {
             tmpHP -= damage;
+            Debug.Log(tmpHP);
         }
         else if(flag==2)
         {
             tmpHP = damage;
+            Debug.Log(tmpHP);
         }
     }
     public float GetHP()
